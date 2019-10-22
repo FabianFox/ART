89074013aad21935b8780f6d46015f64b606f847
FabianFox <fabianguelzau@web.de>
2019-10-22 16:52:28 +0200

    updated

--- data/SZ_Scraper.R
+++ data/SZ_Scraper.R
@@ -2,13 +2,12 @@
 # Project: ART             #
 # 'Reproduktionsmedizin'   #
 # Süddeutsche Zeitung      #
-# 09.05.2018               #
 ############################
 
 # Issues:
 # - None
 
-# Start Docker through PowerShell and initialize a selenium/standalone-firefox:
+# Start Docker through PowerShell and initialize a selenium/stand alone-firefox:
 # In PowerShell:
 # Command: docker run -d -p 4445:4444  selenium/standalone-firefox:3
 # Find: docker ps
@@ -19,8 +18,7 @@
 if (!require("pacman")) install.packages("pacman")
 p_load(RSelenium, tidyverse, rvest, stringr)
 
-# Search for articles in the archive of "SZ"
-# Search term: "Reproduktionsmedizin"
+# Initialize Selenium
 # ---------------------------------------------------------------------------- #
 
 # Initialize the RSelenium server running firefox
@@ -29,9 +27,10 @@ remDr <- RSelenium::rsDriver(remoteServerAddr = "localhost", port = 8887L, brows
 # Open the client to steer the browser
 rD <- remDr[["client"]]
 
-# Navigate to the SZ
+# Navigate to the SZ & configure
+# ---------------------------------------------------------------------------- #
 # Search for: Reproduktionsmedizin*
-# from 02.01.1992 (earliest) to 31.12.2017
+# from 02.01.1990 to 31.12.2017
 # only in the printed issues of "Süddeutsche Zeitung"
 
 # Go to the homepage
@@ -67,25 +66,81 @@ no_supp$clickElement()
 
 # not "Bayern"
 no_bavaria <- rD$findElement("css", "#DIZBY")
-no_bavaria <- rD$clickElement()
+no_bavaria <- no_bavaria$clickElement()
 
 # not "Überregionale"
 no_int <- rD$findElement("css", "#DIZUEREG")
-no_int <- rD$clickElement()
+no_int <- no_int$clickElement()
 
 # Search
 search_btn <- rD$findElement("css", '#searchBtn')
 search_btn$clickElement()
 
-# UNDER CONSTRUCTION
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
+# Save the content
 # ---------------------------------------------------------------------------- #
 
+# Number of pages
+num_articles <- read_html(rD$getPageSource()[[1]]) %>%
+  html_nodes(".searchResultHits") %>%
+  html_text() %>%
+  str_extract(., "[:digit:]+") %>%
+  strtoi()
+  
+# Round
+num_pages <- ceiling(num_articles / 40)
+
+# Move over pages and extract meta-information
+# ---------------------------------------------------------------------------- #
+
+# This section needs updating.
+
+# meta
+get_meta <- function(x){
+  read_html(rD$getPageSource()[[1]]) %>%
+    html_nodes(".hitInfo1 span") %>%
+    html_text()
+}
+
+jump_next <- function(x){
+  click_next <- rD$findElement("css", ".iconGoNext")
+  click_next$clickElement()
+}
+
+jump.df <- tibble(
+  times = 1:10
+)
+
 # Get the articles
-# ------------------------------------------------ #
+# ---------------------------------------------------------------------------- #
+url <- "https://archiv.szarchiv.de/Portal/restricted/Fulltext.act?index="
+
+link.df <- tibble(
+  links = paste0("https://archiv.szarchiv.de/Portal/restricted/Fulltext.act?index=", 
+                 seq(num_articles - 1, 0))
+)
+
+# Functions
+sz_navigate <- function(x){
+  rD$navigate(x) 
+  rD$getPageSource()[[1]]
+}
+
+sz_scrape <- function(x){
+  read_html(x) %>%
+    html_nodes("#articleTextContainer") %>%
+    html_text()
+}
+
+sz_navigate <- possibly(sz_navigate, NA_real_)
+sz_scrape <- possibly(sz_scrape, NA_real_)
+
+test <- map(link.df$links[11:14], ~ {
+  Sys.sleep(sample(seq(2, 5, by = .5), 1))   # friendly scraping
+  url <- sz_navigate(.x)
+  sz_scrape(url)
+})
+
+# (yet to be integrated) PARTS
 # (1) Kommentar
 # Open article type window
 articleType <- rD$findElement("css", "#Numeric_Box6 .menuItemSub")
@@ -97,68 +152,3 @@ comment$clickElement()
 
 SZcomment <- vector(mode = "character", length = 18)
 SZcommentMeta <- vector(mode = "character", length = 18)
-
-# Meta information
-SZcommentMeta <- read_html(rD$getPageSource()[[1]]) %>%
-  html_nodes(".hitInfo") %>%
-  html_text() %>%
-  rev() # I scrape from oldest to youngest below
-
-# The actual loop (articles)
-for(i in seq(0, 18)) {
-  print(paste0("Scraping article ", i, " of ", length(SZcomment)))
-  num <- i + 1
-  rD$navigate(paste0("https://archiv.szarchiv.de/Portal/restricted/Fulltext.act?index=", i))
-  content <- read_html(rD$getPageSource()[[1]])
-  
-  SZcomment[[num]] <- content %>%
-    html_nodes(css = "p") %>%
-    html_text() %>%
-    paste0(., collapse = " ")
-  
-  Sys.sleep(sample(seq(0, 5, 0.5), 1))
-}
-
-# ------------------------------------------------ #
-
-# (2) Interview
-# Open article type window
-articleType <- rD$findElement("css", "#Numeric_Box6 .menuItemSub")
-articleType$clickElement()
-
-# Choose: Interview
-interview <- rD$findElement("css", "#NumericBox_ARTIKELTYPID_content_unselected > div:nth-child(6)")
-interview$clickElement()
-
-SZinterview <- vector(mode = "character", length = 17)
-SZinterviewMeta <- vector(mode = "character", length = 17)
-
-# Meta information
-SZinterviewMeta <- read_html(rD$getPageSource()[[1]]) %>%
-  html_nodes(".hitInfo") %>%
-  html_text() %>%
-  rev() # I scrape from oldest to youngest below
-
-# The actual loop
-for(i in seq(0, 17)) {
-  print(paste0("Scraping article ", i, " of ", length(SZinterview)))
-  num <- i + 1
-  rD$navigate(paste0("https://archiv.szarchiv.de/Portal/restricted/Fulltext.act?index=", i))
-  content <- read_html(rD$getPageSource()[[1]])
-  
-  SZinterview[[num]] <- content %>%
-    html_nodes(css = "p") %>%
-    html_text() %>%
-    paste0(., collapse = " ")
-  
-  Sys.sleep(sample(seq(0, 5, 0.5), 1))
-}
-
-# Put together into a data frame
-SZcorpus <- tibble(
-  type = c(rep("comment", length(SZcomment)), rep("interview", length(SZinterview))),
-  data = c(SZcomment, SZinterview),
-  meta = c(SZcommentMeta, SZinterviewMeta)
-)
-
-# saveRDS(object = SZcorpus, file = "./output/SZcorpus.RDS")
